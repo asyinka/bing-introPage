10d24d801f19507d640b6a20d25f9e1630b7a00b
Yinka <user.asyinka@gmail.com>
2023-03-02 01:01:59 +0100

    the Xmark icon can now close the bing__prevSection

--- bing.js
+++ bing.js
@@ -2,22 +2,30 @@ let shareIcon = document.querySelector("#shareIcon__ctrl");
 let shareCard = document.querySelector('.shareCard');
 let copyLink = document.querySelector('#copyLink');
 let copyMsg = document.querySelector('#shareCard__msg');
+let closeIcon = document.querySelector("#close__icon");
+let bingprevSec = document.querySelector('.bing__prevSection');
 
 function displayShareCard() {
-    shareCard.style.display = 'block';
+        shareCard.style.display = 'block';
 }
 function offdisplayShareCard() {
     shareCard.style.display = 'none';
+    copyMsg.innerText = 'Introducing the new bing';
+    copyMsg.style.color = 'black';
 }
 
 copyLink.addEventListener('click', () => {
-    let para = document.createElement('p');
-    para.setAttribute('id', 'shareCard__msg');
-    let textNode = document.createTextNode('Link copied to clipboard');
-    para.appendChild(textNode);
-
-    copyMsg.innerHTML = para;
-    shareCard__msg.style.display = 'none';
-    displayShareCard();
+        copyMsg.innerText = 'Link copied to clipboard';
+        copyMsg.style.color = 'green';
+    // displayShareCard();
 })
+
+// closeIcon.onclick() = () => {
+//     bingprevsec.style.display = 'none';
+// }
+closeIcon.addEventListener('click', closeBingPrev);
+
+function closeBingPrev(){
+    bingprevSec.style.display = 'none';
+}
 
